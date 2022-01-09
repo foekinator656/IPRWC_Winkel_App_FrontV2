c77e796790447cc4f759a595026e80e42d556ace
loek <loek@appel-snijder.nl>
2022-01-09 20:02:48 +0100

    als admin alle orders zien

--- src/app/manage/manage-orders/order-reporting.service.ts
+++ src/app/manage/manage-orders/order-reporting.service.ts
@@ -3,6 +3,7 @@ import {BikeOrder} from "../../shared/models/bike-order.model";
 import {Bike} from "../../shared/models/bike.model";
 import {HttpClient} from "@angular/common/http";
 import {ApiService} from "../../shared/api.service";
+import {AuthService} from "../../shared/auth.service";
 
 @Injectable({
   providedIn: 'root'
@@ -16,14 +17,14 @@ export class OrdersService {
   errorMessage!: string;
   delay = (ms: number) => new Promise(res => setTimeout(res, ms));
 
-  constructor(private http: HttpClient,private apiService: ApiService) { }
+  constructor(private http: HttpClient,private apiService: ApiService,private authService: AuthService) { }
 
 
   async fetchBikeOrders() {
-    this.http.get<BikeOrder[]>(this.apiService.apiUrl+'bikeorder')
+    let bikeOrdersReceived = false;
+    this.http.post<BikeOrder[]>(this.apiService.apiUrl+'bikeorder',this.authService.authenticatedUser)
       .subscribe(bikeOrders => {
         this.bikeOrders = [];
-        console.log(bikeOrders);
         this.bikeOrders = bikeOrders.slice();
         if (this.bikeOrders.length > 0 ){
           this.getOrdersDone = true;
@@ -35,34 +36,19 @@ export class OrdersService {
         console.log(error);
         this.errorMessage = error;
       });
-    while (!this.getOrdersDone) {
-         await this.delay(1000);
+    while (!bikeOrdersReceived) {
+         await this.delay(10);
     }
-
     for (let bikeOrder of this.bikeOrders) {
       this.bikeOrderPrices.push(await this.getPriceForOrder(bikeOrder));
     }
-    console.log()
   }
 
   async getPriceForOrder(bikeOrder: BikeOrder){
-    while (!this.getOrdersDone) {
-      await this.delay(2000);
-    }
-    while (this.bikeOrders === undefined) {
-      await this.delay(2000);
-    }
-    console.log("binnen getPriceForOrder");
-    console.log("111 ********************" + bikeOrder);
-    let orderId = bikeOrder.orderId;
-    console.log("222 ********************" + bikeOrder.orderId);
-    console.log(bikeOrder.orderId);
-    console.log("333 ********************" + orderId);
     let bikesReceived = false;
     let totalprice: number = 0;
     this.http.get<Bike[]>(this.apiService.apiUrl+'bike/bikeOrder/'+bikeOrder.orderId)
       .subscribe(bikesAPI => {
-        console.log(bikesAPI);
         this.bikes = bikesAPI;
         bikesReceived = true;
       }, error => {
@@ -70,9 +56,8 @@ export class OrdersService {
         this.errorMessage = error;
       });
     while (!bikesReceived) {
-      await this.delay(2000);
+      await this.delay(10);
     }
-    console.log(this.bikes);
     for (let bike of this.bikes){
       totalprice += bike.price;
     }
@@ -81,14 +66,12 @@ export class OrdersService {
 
   getAllBikes(){
     this.http.get<Bike[]>(this.apiService.apiUrl+'bike')
-      .subscribe(bikes => {
-        console.log(bikes);
-        this.bikes = bikes.slice();
-        console.log(this.bikeOrders);
-        return bikes;
-      }, error => {
-        console.log(error);
-        this.errorMessage = error;
-      });
+    .subscribe(bikes => {
+      this.bikes = bikes.slice();
+      return bikes;
+    }, error => {
+      console.log(error);
+      this.errorMessage = error;
+    });
   }
 }
